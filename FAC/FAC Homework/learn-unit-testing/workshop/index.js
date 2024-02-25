361d9afa1d2a3af60c7ed65f80188d593eb758b8
EmilRosenov <79119174+EmilRosenov@users.noreply.github.com>
2024-02-25 22:29:02 +0000

    committing learn-unit-testing

---
+++ FAC/FAC Homework/learn-unit-testing/workshop/index.js
@@ -0,0 +1,13 @@
+export default function makeUrl(name) {
+  return "https://pokeapi.co/api/v2/" + name;
+}
+
+
+export function isLeapYear(year) {
+  if (typeof year !== "number") return "Please enter a number";
+  if (year < 0) return "Year cannot be negative";
+  if (year % 400 === 0) return true; // 2000 was a leap year
+  if (year % 100 === 0) return false; // 1900 was not
+  if (year % 4 === 0) return true; // 2020 was
+  return false; // all other years are not leap years
+}
